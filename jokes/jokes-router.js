072a0db4eb43fe5075d3746b66bd24842a64fc0b
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2021-01-23 13:03:20 -0600

    jokes-router written

--- jokes/jokes-router.js
+++ jokes/jokes-router.js
@@ -1,16 +1,18 @@
 const axios = require('axios');
+const {authenticate} = require("../auth/authenticate-middleware")
 
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
+router.get('/', authenticate(), async (req, res, next) => {
   const requestOptions = {
     headers: { accept: 'application/json' },
   };
 
-  axios
+  await axios
     .get('https://icanhazdadjoke.com/search', requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
+      next()
     })
     .catch(err => {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
