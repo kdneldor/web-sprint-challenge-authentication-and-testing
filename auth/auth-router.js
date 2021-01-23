b90131f323bb2ed4e9e74b06682c4f6a4962a880
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2021-01-23 12:57:29 -0600

    auth-router written

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,53 @@
 const router = require('express').Router();
+const bcrypt = require("bcryptjs");
+const Users = require("./auth-model")
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post('/register', (req, res, next) => {
+  try {
+    const {username, password} = req.body;
+    const user = await Users.findBy({username}).first()
+
+    if (user) {
+      return res.status(409).json({
+        message: "Username is already taken",
+      })
+    }
+    const newUser = await Users.add({
+      username, 
+      password: await bcrypt.hash(password, 10)
+    })
+    res.status(201).json(newUser)
+  } catch (err){
+    next(err)
+  }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post('/login', (req, res, next) => {
+  try {
+    const {username, password} = req.body
+    const user = await Users.findBy({username}).first()
+
+    if(!user) {
+      return res.status(401).json({
+        message: "Invalid Credentials",
+      })
+    }
+    const validPassword = await bcrypt.compare(password, user.password)
+    
+    if (!validPassword) {
+      return res.status(401).json({
+        message: "Invalid Credentials", 
+      })
+    }
+
+    req.session.user = user
+    
+    res.json({
+      message: `Welcome ${user.username}`
+    })
+  } catch (err){
+    next(err)
+  }
 });
 
 module.exports = router;
